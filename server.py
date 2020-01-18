690e836870dfbeb649f87a489660d7e0e22204e6
kirikoto <59285270+kirikoto@users.noreply.github.com>
2020-01-19 05:33:44 +0800

    Server
    mySQL and python

---
+++ server.py
@@ -0,0 +1,17 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="ethan",
+  database="mydatabase"
+)
+
+mycursor = mydb.cursor()
+
+sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
+val = ("John", "Highway 21")
+mycursor.execute(sql, val)
+
+mydb.commit()
+
+print(mycursor.rowcount, "record inserted.")
